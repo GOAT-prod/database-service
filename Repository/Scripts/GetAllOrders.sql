0c3729eaecd9553f74b0ba0d62a78f4da55b30ea
ducknes <ilya.anton0v@vk.com>
2024-12-22 16:53:33 +0300

    fix script

--- Repository/Scripts/GetAllOrders.sql
+++ Repository/Scripts/GetAllOrders.sql
@@ -7,9 +7,9 @@ select o.id            as Id,
        sum(pri.weight) as TotalWeight,
        sum(od.amount)  as TotalPrice
 from orders o
-         join users u on u.id = o.user_id
-         join orders_item oi on o.id = oi.order_id
-         join product_item pri on oi.product_item_id = pri.id
-         join operation op on o.id = op.order_id
-         join operation_detail od on op.id = od.operation_id
+         left join users u on u.id = o.user_id
+         left join orders_item oi on o.id = oi.order_id
+         left join product_item pri on oi.product_item_id = pri.id
+         left join operation op on o.id = op.order_id
+         left join operation_detail od on op.id = od.operation_id
 group by o.id, o.type, o.status, o.create_date, o.delivery_date, u.username;
